e31f071205d05b6f3b0afaba73a40b5717591440
Matthew Hartley <mhartley@cantab.net>
2015-02-11 09:05:11 +0000

    Moved and updated test_blueprint

--- clustack/tests/test_blueprint.py
+++ clustack/tests/test_blueprint.py
@@ -1,35 +1,35 @@
 import unittest
 
-import blueprint
+import clustack.blueprint
 
 class TestBluePrint(unittest.TestCase):
 
     def test_get_available_blueprints(self):
-        avail = blueprint.get_available_blueprints()
+        avail = clustack.blueprint.get_available_blueprints()
 
         self.assertIn('zlib', avail)
 
     def test_load_blueprint_yaml(self):
-        bp_zlib = blueprint.load_blueprint_yaml('yaml/zlib.yaml')
+        bp_zlib = clustack.blueprint.load_blueprint_yaml('yaml/zlib.yaml')
 
         self.assertEqual(bp_zlib.name, 'zlib')
 
     def test_load_blueprint(self):
-        bp_zlib = blueprint.load_blueprint('yaml/zlib.yaml')
+        bp_zlib = clustack.blueprint.load_blueprint('yaml/zlib.yaml')
 
         self.assertEqual(bp_zlib.name, 'zlib')
 
     def test_load_blueprint_by_name(self):
-        bp_python = blueprint.load_blueprint_by_name('python')
+        bp_python = clustack.blueprint.load_blueprint_by_name('python')
 
         self.assertEqual(bp_python.name, 'python')
 
         self.assertEqual(bp_python.direct_dependencies, ['openssl', 'readline'])
 
     def test_full_dependencies(self):
-        bp_python = blueprint.load_blueprint_by_name('python')
+        bp_python = clustack.blueprint.load_blueprint_by_name('python')
 
-        expected_deps = ['readline', 'ncurses', 'openssl', 'perl']
+        expected_deps = ['readline', 'ncurses', 'openssl']
         
         self.assertEqual(bp_python.full_dependencies, expected_deps)
 
